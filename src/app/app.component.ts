3306bf2a64129360827642b19faead4dbcc6df55
Stefano Ramirez <ramirez.stefano@gmail.com>
2024-06-05 23:34:45 -0600

    update header component with nice css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ColorPickerPopoverComponent } from './color-picker-popover/color-picker-popover.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ColorPickerPopoverComponent],
+  imports: [RouterOutlet, ColorPickerPopoverComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'signatureGenerator';
